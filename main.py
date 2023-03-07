c483a214bf8c59a8eda7e810de124246e1679040
Sergio Arenas Moreno <sam@foryouandyourcustomers.com>
2023-03-07 20:21:50 +0100

    Cleaning in order to make it run constantly for a single host

--- main.py
+++ main.py
@@ -1,53 +1,38 @@
-from pythonping import ping
 from datetime import datetime, timedelta
 import subprocess
 import time
 import csv
-import random
 import sys
-import concurrent.futures
+import re
 
-append_each = 60000  # In milliseconds
+append_each = 60  # In seconds
 delay = 500  # Delay between checks in milliseconds
 timeout = 200  # Request timeout in milliseconds
-
-
 host = [sys.argv[1], sys.argv[2]]
-host_status = []
 
 
-def check_ping(target):
-    # return not random.randint(1, 100) <= 30
-    # print(subprocess.check_output(['ping', '8.8.8.8', '-f', '-c', '10']))
-    return "Timed out" not in str(
-        ping(timeout=timeout / 1000, count=1, target=target, out=None, out_format=None)._responses[0])
+def ping_rate(target):
+    try:
+        to = str(timeout / 1000).replace('.', ',')
+        d = str(delay / 1000).replace('.', ',')
+        a = subprocess.check_output(['ping', target, '-w', str(append_each), '-W', to, '-i', d, '-q'])
+        return (re.search(r'(\d+(?:[.,]\d+)?)(?=% packet loss,)', str(a))).group(1)
+    except:
+        print("ERROR")
+        return 0
 
 
-def append_to_csv(status, m, file_name):
-    rate = sum(1 for value in status if not value) / len(status)
+def append_to_csv(rate, m, file_name):
     with open(file_name, 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([m, '%.4f' % rate])
-
-
-def ping_request():
-    result = check_ping(host[0])
-    host_status.append(result)
+        writer.writerow([m, rate])
 
 
 if __name__ == '__main__':
-    next_time = datetime.now().replace(microsecond=0)
-    next_append = next_time.replace(second=0) + timedelta(milliseconds=append_each - delay)
+    next_time = datetime.now().replace(microsecond=0, second=0) + timedelta(minutes=1)
 
     while True:
         if datetime.now() >= next_time:
-
-            next_time = next_time + timedelta(milliseconds=delay)
-            ping_request()
-
-            if datetime.now() >= next_append:
-                time_of_log = next_append.replace(second=0)
-                append_to_csv(host_status, time_of_log, host[1])
-                host_status = []
-                next_append = next_time + timedelta(milliseconds=append_each - delay)
+            append_to_csv(ping_rate(host[0]), next_time, host[1])
+            next_time = next_time + timedelta(seconds=append_each)
         time.sleep(0.01)
